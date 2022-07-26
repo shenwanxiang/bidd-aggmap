522e1d3f3ec84e05688a28ff0ef281b4386f2521
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2022-07-26 17:01:44 +0800

    update index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ release = '1.1.1'
 
 extensions = []
 
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,5 +25,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
+#html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
