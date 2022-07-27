3d702f9cf13a99acb649baee0b15a03b01b56426
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2022-07-27 14:06:08 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,11 @@ sys.path.insert(0, os.path.abspath('.'))
 #
 # needs_sphinx = '1.0'
 
+master_doc = "index"
+source_suffix = ['.rst', '.md']
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".ipynb_checkpoints"]
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -39,6 +44,7 @@ extensions = [
     #    'bokeh.sphinxext.bokeh_plot',
     "sphinx_gallery.gen_gallery",
 ]
+
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
